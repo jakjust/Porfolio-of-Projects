807c8ffb7e4aebcde68a136a001db30d51260e8b
jakjust <jakub.j2652@gmail.com>
2025-05-11 20:36:14 +0200

    Add files via upload

--- BSmodel.py
+++ BSmodel.py
@@ -6,8 +6,9 @@ import plotly.graph_objects as go
 from numpy import log, sqrt, exp 
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-
+import sympy as sp
+import yfinance as yf
+from autograd import grad
 
 
 def bsm_call(S, K, T, r, sigma):
@@ -32,17 +33,108 @@ def bsm_greeks(S, K, T, r, sigma):
     d1 = (np.log(S/K) + (r + 0.5 * sigma ** 2) * T)/(sigma * np.sqrt(T))
     d2 = d1 - sigma * np.sqrt(T)
     
-    delta_call = norm.cdf(d1)
-    delta_put = norm.cdf(d1) - 1
-    gamma = norm.pdf(d1) / (S * sigma * np.sqrt(T))
-    theta_call = (-S * norm.pdf(d1) * sigma / (2 * np.sqrt(T)) - r * K * np.exp(-r * T) * norm.cdf(d2))
-    theta_put = (-S * norm.pdf(d1) * sigma / (2 * np.sqrt(T)) + r * K * np.exp(-r * T) * norm.cdf(-d2))
-    vega = S * norm.pdf(d1) * np.sqrt(T)
+    delta_call = sp.diff(bsm_call(S, K, T, r, sigma), S).evalf()
+    delta_put = sp.diff(bsm_put(S, K, T, r, sigma), S).evalf()
+    gamma = sp.diff(delta_call, S).evalf()
+    theta_call = sp.diff(bsm_call(S, K, T, r, sigma), T).evalf()
+    theta_put = sp.diff(bsm_put(S, K, T, r, sigma), T).evalf()
+    vega = sp.diff(bsm_call(S, K, T, r, sigma), sigma).evalf()
+    rho_call = sp.diff(bsm_call(S, K, T, r, sigma), r).evalf()
+    rho_put = sp.diff(bsm_put(S, K, T, r, sigma), r).evalf()
     
-    return delta_call, delta_put, gamma, theta_call, theta_put, vega
+    return delta_call, delta_put, gamma, theta_call, theta_put, vega, rho_call, rho_put 
+
+
+ticker = yf.Ticker("AAPL")
+expirations = ticker.options
+opt_date = expirations[2]   
+opt_chain = ticker.option_chain(opt_date)   
+calls = opt_chain.calls
+calls = calls[(calls["impliedVolatility"] > 0.01)]
+puts = opt_chain.puts   
+puts = puts[(puts["impliedVolatility"] > 0.01)] 
+
+
+def expiration_date(contractSymbol):
+    date = contractSymbol[4:10]
+    year = '20' + date[0:2]
+    month = date[2:4]
+    day = date[4:6]
+    return f"{year}-{month}-{day}"
+
+calls["expirations"] = calls["contractSymbol"].apply(expiration_date)
+puts["expirations"] = puts["contractSymbol"].apply(expiration_date)
+
+calls["timeToExpiration"] = (pd.to_datetime(calls["expirations"]) - pd.to_datetime("today")).dt.days /365
+puts["timeToExpiration"] = (pd.to_datetime(puts["expirations"]) - pd.to_datetime("today")).dt.days /365
 
 
 
+
+
+
+
+def loss_call(S, K, T, r, sigma_guess, price):
+    theorietical_price = bsm_call(S, K, T, r, sigma_guess) 
+    
+    actual_price = price
+    
+    
+    return theorietical_price - actual_price
+
+
+
+def loss_put(S, K, T, r, sigma_guess, price):
+    theorietical_price = bsm_put(S, K, T, r, sigma_guess) 
+    
+    actual_price = price
+    
+    
+    return theorietical_price - actual_price
+
+
+
+
+
+
+loss_grad = grad(loss_call, argnums=4)
+
+def solve_iv_call(S, K, T, r, price, sigma_guess=0.2, 
+             N_iter = 20, epsilon = 0.001, verbose = True):
+    simga = sigma_guess
+    for i in range(N_iter):
+        loss_val = loss_call(S, K, T, r, sigma, price)
+        
+        if abs(loss_val) < epsilon:
+            break
+        else:
+            loss_grad_val = loss_grad(S, K, T, r, sigma, price)
+            sigma = sigma - loss_val/loss_grad_val
+    return sigma
+
+loss_grad = grad(loss_put, argnums=4)
+
+def solve_iv_put(S, K, T, r, price, sigma_guess=0.2,
+             N_iter = 20, epsilon = 0.001, verbose = True):
+    simga = sigma_guess
+    for i in range(N_iter):
+        loss_val = loss_put(S, K, T, r, sigma, price)
+        
+        if abs(loss_val) < epsilon:
+            break
+        else:
+            loss_grad_val = loss_grad(S, K, T, r, sigma, price)
+            sigma = sigma - loss_val/loss_grad_val
+    return sigma
+
+
+
+
+            
+        
+
+
+    
 
 st.set_page_config(page_title="Black-Scholes Option Pricing", layout="wide")
 st.title("Black-Scholes Option Pricing Model")
@@ -60,13 +152,19 @@ if st.button("Calculate Option Prices"):
    
     call_price = bsm_call(S, K, T, r, sigma)
     put_price = bsm_put(S, K, T, r, sigma)
+    impliedvolatilitycall = solve_iv_call(S, K, T, r,)
+    impliedvolatilityput = solve_iv_put(S, K, T, r,)
 
     st.subheader("Option Prices")
     st.write(f"**Call Option Price:** ${call_price:.2f}")
     st.write(f"**Put Option Price:** ${put_price:.2f}")
+    
+    st.subheader("Implied Volatility")
+    st.write(f"**Implied Volatility (Call):** {impliedvolatilitycall:.4f}")
+    st.write(f"**Implied Volatility (Put):** {impliedvolatilityput:.4f}")
 
     
-    delta_call, delta_put, gamma, theta_call, theta_put, vega = bsm_greeks(S, K, T, r, sigma)
+    delta_call, delta_put, gamma, theta_call, theta_put, vega, rho_call, rho_put = bsm_greeks(S, K, T, r, sigma)
 
     st.subheader("Option Greeks")
     st.markdown(f"- **Delta (Call):** {delta_call:.4f}")
@@ -75,6 +173,8 @@ if st.button("Calculate Option Prices"):
     st.markdown(f"- **Theta (Call):** {theta_call:.4f}")
     st.markdown(f"- **Theta (Put):** {theta_put:.4f}")
     st.markdown(f"- **Vega:** {vega:.4f}")
+    st.markdown(f"- **Rho (Call):** {rho_call:.4f}")
+    st.markdown(f"- **Rho (Put):** {rho_put:.4f}")
 
     st.subheader("Call & Put Prices vs. Stock Price")
 
